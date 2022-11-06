b0c8228beb529827eef2bdad015f4af93bb02904
AbdulAdal <k21007037@kcl.ac.uk>
2022-11-06 16:35:17 +0000

    Implement things 1 solution

--- things/models.py
+++ things/models.py
@@ -1,8 +1,7 @@
-# from unittes...t.ut......il import _MAX_LENGTH
+
 from django.core.validators import MaxValueValidator,MinValueValidator, MaxLengthValidator
 from django.db import models
-# from django.db.models.Field import validators
-# from django.db.models import fields
+
 
 
 class Thing(models.Model):
